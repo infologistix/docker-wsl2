360122dc35779cfe8efc5226bf4349b7201c46b9
paul <paul@DESKTOP-ES4JF5S.localdomain>
2022-01-20 11:50:39 +0100

    fix uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -4,25 +4,19 @@ remove_install(){
     ## debian based
     if [[ $(command -v apt) = ""]]; then
     echo -e "This is Debian-based\nRemoving with apt..."
-    sudo apt autoremove -y docker
+    sudo apt autoremove -y docker-ce docker-ce-client containerd.io
     fi
     # suse based
     if [[ $(command -v zypper) = ""]]; then
     echo -e "This is OpenSUSE\nRemoving with zypper..."
     sudo zypper remove -y docker
     fi
-    # arch based
-    if [[ $(command -v pacman) = ""]]; then
-    echo -e "This is Arch\nRemoving with pacman..."
-    sudo pacman -R --noconfirm docker
-    fi
 }
 
 remove_files(){
 sudo rm /etc/docker/daemon.json
-sudo rm /etc/init.d/dockerd
-sudo rm /etc/profile.d/zzz-zzz-dockerd.sh
-rm $HOME/bin/docker-service
+sudo rm /etc/init.d/docker
+sudo rm /etc/profile.d/zzz-zzz-docker.sh
 sudo sed -i '%docker ALL=(ALL) NOPASSWD: \/usr\/bin\/dockerd/d' /etc/sudoers
 sudo sed -i '%users ALL=(ALL) NOPASSWD: \/usr\/sbin\/service dockerd*/d' /etc/sudoers
 }
