91d99c44963d02bb2e3a65dabc589bf781d393a5
pickmylight <paul_schmidt@mail.de>
2022-01-20 13:13:35 +0100

    fix ubuntu

--- install.sh
+++ install.sh
@@ -4,20 +4,20 @@ PATH=$PATH:/mnt/c/Windows/system32:/mnt/c/windows/system32/WindowsPowershell/v1.
 
 debian(){
     # this installs even the service for us...
-    sudo apt install ca-certificates gnupg lsb-release
+    sudo apt install -y ca-certificates gnupg lsb-release
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
-    sudo apt install docker-ce docker-ce-client containerd.io
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
 }
 
 ubuntu(){
     # this installs even the service for us...
-    sudo apt install ca-certificates gnupg lsb-release
+    sudo apt install -y ca-certificates gnupg lsb-release
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
-    sudo apt install docker-ce docker-ce-client containerd.io
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
 }
 
 install_docker(){
